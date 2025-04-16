dcad4b799f50998144900bdee622e0fb02fb36c5
Honesty2022 <honestyfdschool@gmail.com>
2025-04-16 19:28:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+import os
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! This is Honesty School Bot.")
